01ee1b6ef9b24a31cc726c8a52cdb616a8e78780
HenryTaylor2019 <henrytaylor@live.co.uk>
2020-03-23 14:01:08 +0000

    Third Commit - Project Restructure. Header and Footer comps added

--- src/App.js
+++ src/App.js
@@ -4,22 +4,29 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
-import Navbar from "./Navbar";
-// import Footer from "./Footer";
-import Find1 from "./Find1"
+
+
+import Navbar from "./Components/Navbar";
+import GamePage from "./GamePage.js"
+import Footer from "./Components/Footer.js"
+
 const App = () => (
-  
+
   <Router>
-    
-      <Fragment>
-        <Navbar></Navbar>
-     
-        <Switch>
-          <Route exact path="/find/1" component={ Find1 } />
-          <Route exact path="/find/2" component={ Find1 } />
-        </Switch>
-        {/* <Footer></Footer> */}
-      </Fragment>
-  </Router>  
+
+    <Fragment>
+
+      <Navbar />
+
+      <Switch>
+        <Route path="/gamepage">
+          <GamePage />
+        </Route>
+      </Switch>
+
+        <Footer />
+
+    </Fragment>
+  </Router>
 );
 export default App;
