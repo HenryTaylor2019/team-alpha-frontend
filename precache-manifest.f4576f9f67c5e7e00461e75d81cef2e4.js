e1b0c566d6dd2199278a588d6eb86a7d97329958
HenryTaylor2019 <henrytaylor@live.co.uk>
2020-03-26 16:53:43 +0000

    Updates

---
+++ precache-manifest.f4576f9f67c5e7e00461e75d81cef2e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3de2e42cb5c491335359f929bd4c9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c0bfc94d7a223528d7",
+    "url": "/static/js/2.8f0970cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8f0970cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "732b7081e7374c207633",
+    "url": "/static/js/main.adfc9be5.chunk.js"
+  },
+  {
+    "revision": "7b9caf9e33c0d11d7454",
+    "url": "/static/js/runtime-main.ae0f25c3.js"
+  }
+]);
