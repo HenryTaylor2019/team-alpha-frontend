a17fec8db4b135edf7121190bae05c54359cad72
HenryTaylor2019 <henrytaylor@live.co.uk>
2020-03-27 11:45:13 +0000

    Updates

---
+++ precache-manifest.198634514db1b3be02e2c72fdd417a37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17395eb9b2f5a18224f8d6969d04606",
+    "url": "/team-alpha-frontend/index.html"
+  },
+  {
+    "revision": "9c107092e49ec88321de",
+    "url": "/team-alpha-frontend/static/js/2.715446ba.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/team-alpha-frontend/static/js/2.715446ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0af769d04a951dbc9d5e",
+    "url": "/team-alpha-frontend/static/js/main.1e3ae792.chunk.js"
+  },
+  {
+    "revision": "1caf270058de77045cf2",
+    "url": "/team-alpha-frontend/static/js/runtime-main.6d64ed6c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd7ac9cd73b834cd6b3120c21c5ac2be.js"
+  "/team-alpha-frontend/precache-manifest.198634514db1b3be02e2c72fdd417a37.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/team-alpha-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
