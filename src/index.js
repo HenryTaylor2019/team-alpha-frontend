51531b46b226dcd6cd6f8d331ddb3680a4f65412
HenryTaylor2019 <henrytaylor@live.co.uk>
2020-03-27 11:38:30 +0000

    axois installed. App.js redirected

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './data/store';
 
-import App from './App';
+import App from './Components/App';
 import { Provider } from 'react-redux';
 
 
