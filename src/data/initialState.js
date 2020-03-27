fd263f8e01288f4e45ad68ab4896433a65e707fd
HenryTaylor2019 <henrytaylor@live.co.uk>
2020-03-27 14:20:39 +0000

    removed dummy data. Deployment operational

--- src/data/initialState.js
+++ src/data/initialState.js
@@ -1,99 +1,98 @@
 const initialState = {
   counter: 1,
   score: 1,
-  images: [
-    {
-      "id": 1,
-      "title": "Image 1",
-      "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p1_facetouch.jpg?raw=true",
-      "top": '30%',
-      "left": '40%',
-      "bottom": '35%',
-      "right": '37%'
-    },
-    {
-      "id": 2,
-      "title": "Image 2",
-      "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p2_handshake2.jpg?raw=true",
-      "top": '44%',
-      "left": '34%',
-      "bottom": '35%',
-      "right": '43%'
-    },
-    {
-      "id": 3,
-      "title": "Image 3",
-      "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p3_sneeze.jpg?raw=true",
-      "top": '20%',
-      "left": '23%',
-      "bottom": '48%',
-      "right": '52%'
-    },
-    {
-      "id": 4,
-      "title": "Image 4",
-      "url": "http://2.bp.blogspot.com/-YOW8JwbseY0/UZKrTEz1zqI/AAAAAAAAADo/JetVFqhzEH4/s1600/Zombie-Chase-zombieland-20358646-1280-1024.jpg",
-      "top": '37%',
-      "left": '65%',
-      "bottom": '15%',
-      "right": '10%'
-    },
-    {
-      "id": 5,
-      "title": "Image 5",
-      "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p5_foodonthefloor.jpg?raw=true",
-      "top": '70%',
-      "left": '3%',
-      "bottom": '3%',
-      "right": '60%'
-    },
-    {
-      "id": 6,
-      "title": "Image 6",
-      "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p6_share.jpg?raw=true",
-      "top": '35%',
-      "left": '30%',
-      "bottom": '34%',
-      "right": '40%'
-    },
-    {
-      "id": 7,
-      "title": "Image 7",
-      "url": "https://www.orissapost.com/wp-content/uploads/2019/02/a1-7-1140x570.jpg",
-      "top": '25%',
-      "left": '40%',
-      "bottom": '30%',
-      "right": '30%'
-    },
-    {
-      "id": 8,
-      "title": "Image 8",
-      "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p8_hug.jpg?raw=true",
-      "top": '20%',
-      "left": '20%',
-      "bottom": '50%',
-      "right": '30%'
-    },
-    {
-      "id": 9,
-      "title": "Image 9",
-      "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p9_touch.jpg?raw=true",
-      "top": '14%',
-      "left": '65%',
-      "bottom": '30%',
-      "right": '14%'
-    },
-    {
-      "id": 10,
-      "title": "Image 10",
-      "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p10_zombi.jpg?raw=true",
-      "top": '10%',
-      "left": '50%',
-      "bottom": '60%',
-      "right": '0%'
-    },
-  ]
+  images: []
 };
 
 
 export default initialState;
+// {
+//   "id": 1,
+//   "title": "Image 1",
+//   "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p1_facetouch.jpg?raw=true",
+//   "top": '30%',
+//   "left": '40%',
+//   "bottom": '35%',
+//   "right": '37%'
+// },
+// {
+//   "id": 2,
+//   "title": "Image 2",
+//   "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p2_handshake2.jpg?raw=true",
+//   "top": '44%',
+//   "left": '34%',
+//   "bottom": '35%',
+//   "right": '43%'
+// },
+// {
+//   "id": 3,
+//   "title": "Image 3",
+//   "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p3_sneeze.jpg?raw=true",
+//   "top": '20%',
+//   "left": '23%',
+//   "bottom": '48%',
+//   "right": '52%'
+// },
+// {
+//   "id": 4,
+//   "title": "Image 4",
+//   "url": "http://2.bp.blogspot.com/-YOW8JwbseY0/UZKrTEz1zqI/AAAAAAAAADo/JetVFqhzEH4/s1600/Zombie-Chase-zombieland-20358646-1280-1024.jpg",
+//   "top": '37%',
+//   "left": '65%',
+//   "bottom": '15%',
+//   "right": '10%'
+// },
+// {
+//   "id": 5,
+//   "title": "Image 5",
+//   "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p5_foodonthefloor.jpg?raw=true",
+//   "top": '70%',
+//   "left": '3%',
+//   "bottom": '3%',
+//   "right": '60%'
+// },
+// {
+//   "id": 6,
+//   "title": "Image 6",
+//   "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p6_share.jpg?raw=true",
+//   "top": '35%',
+//   "left": '30%',
+//   "bottom": '34%',
+//   "right": '40%'
+// },
+// {
+//   "id": 7,
+//   "title": "Image 7",
+//   "url": "https://www.orissapost.com/wp-content/uploads/2019/02/a1-7-1140x570.jpg",
+//   "top": '25%',
+//   "left": '40%',
+//   "bottom": '30%',
+//   "right": '30%'
+// },
+// {
+//   "id": 8,
+//   "title": "Image 8",
+//   "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p8_hug.jpg?raw=true",
+//   "top": '20%',
+//   "left": '20%',
+//   "bottom": '50%',
+//   "right": '30%'
+// },
+// {
+//   "id": 9,
+//   "title": "Image 9",
+//   "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p9_touch.jpg?raw=true",
+//   "top": '14%',
+//   "left": '65%',
+//   "bottom": '30%',
+//   "right": '14%'
+// },
+// {
+//   "id": 10,
+//   "title": "Image 10",
+//   "url": "https://github.com/HenryTaylor2019/team-alpha-frontend/blob/f60dbd549c0258a4c803d97b870ec98872d5af03/src/Atomic/img/p10_zombi.jpg?raw=true",
+//   "top": '10%',
+//   "left": '50%',
+//   "bottom": '60%',
+//   "right": '0%'
+// },
